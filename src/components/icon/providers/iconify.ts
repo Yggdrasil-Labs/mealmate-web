f99f3e024cc5579d8bb738107382c82c7b718dd3
YangYang <1059376437@qq.com>
2026-05-30 07:35:26 +0800

    fix(icon): 补充周计划/备菜计划/采购清单缺失的菜单图标
    - 新增 menu-plan(calendar)、menu-prep(knife-fork)、menu-shopping(shopping-cart)
    - 同步更新 SemanticIconName 类型和 iconify 映射

--- src/components/icon/providers/iconify.ts
+++ src/components/icon/providers/iconify.ts
@@ -6,6 +6,7 @@ import type { Component } from 'vue'
 import type { SemanticIconName } from '../icon.types'
 
 import IconEpBack from '~icons/ep/back'
+import IconEpCalendar from '~icons/ep/calendar'
 import IconEpClose from '~icons/ep/close'
 import IconEpDelete from '~icons/ep/delete'
 import IconEpDish from '~icons/ep/dish'
@@ -14,12 +15,14 @@ import IconEpExpand from '~icons/ep/expand'
 import IconEpFold from '~icons/ep/fold'
 import IconEpFolder from '~icons/ep/folder'
 import IconEpInfo from '~icons/ep/info-filled'
+import IconEpKnifeFork from '~icons/ep/knife-fork'
 import IconEpLink from '~icons/ep/link'
 import IconEpAdd from '~icons/ep/plus'
 import IconEpRefresh from '~icons/ep/refresh'
 import IconEpError from '~icons/ep/remove'
 import IconEpSearch from '~icons/ep/search'
 import IconEpSetting from '~icons/ep/setting'
+import IconEpShoppingCart from '~icons/ep/shopping-cart'
 import IconEpSuccess from '~icons/ep/success-filled'
 import IconEpWarning from '~icons/ep/warning-filled'
 
@@ -37,6 +40,9 @@ const iconifyMap: Record<SemanticIconName, Component> = {
   'menu-system': IconEpInfo,
   'menu-user': IconEpSuccess,
   'menu-recipe': IconEpDish,
+  'menu-plan': IconEpCalendar,
+  'menu-prep': IconEpKnifeFork,
+  'menu-shopping': IconEpShoppingCart,
   'refresh': IconEpRefresh,
   'search': IconEpSearch,
   'status-error': IconEpError,
